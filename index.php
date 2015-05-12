3aebb3d5a314e17aa82b0d63b8b8e4a16012a880
Eric Martinez <martinez_eric82@yahoo.com>
2015-05-12 08:26:55 -0700

    completed video 4

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ ini_set('default_socket_timeout', 300);
 session_start();
 
 //Make Constants using defne.
-define('client_ID', '65d48edbb16143519b7096a59f69549c');
+define('clientID', '65d48edbb16143519b7096a59f69549c');
 define('client_Secret', '18caa1e571844a76adf811a0d6e9b0fa');
-define('redirectURL', 'http://localhost/appscsdemyapi/index.php');
+define('redirectURI', 'http://localhost/apiproject/index.php');
 define('ImageDirectory', 'pics/');
 ?>
 
@@ -20,6 +20,6 @@ define('ImageDirectory', 'pics/');
 <!-- Creating a login for people to go and give approval for our web app to acess their Intasgram their Instagram Account
 After getting approval we are now going to have the information so that we can play with it.
  -->
-<a href="https:api.instagram/oauth/authorize/?client_id=<?php echo client_ID; ?>&redirect_url<?php echo redirectURI?>&response_type=code">LOGIN</a>
+<a href="https://api.instagram/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI?>&response_type=code">LOGIN</a>
 </body>
-</html>
+</htmiL
