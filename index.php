15d2cf7f75cd95fb34a979f5f3f7ccc8e475f72a
Eric Martinez <martinez_eric82@yahoo.com>
2015-05-12 20:15:36 -0700

    completed video 6

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ define('clientSecret', '18caa1e571844a76adf811a0d6e9b0fa');
 define('redirectURI', 'http://localhost/apiproject/index.php');
 define('ImageDirectory', 'pics/');
 
-if isset(($_GET['code'])){
+if (isset($_GET['code'])){
 	$code = ($_GET['code']);
 	$url = 'https://api.instagrm.com/oauth/access_token';
 	$access_token_settings = array('client_id' => clientID,
@@ -32,6 +32,6 @@ if isset(($_GET['code'])){
 <!-- Creating a login for people to go and give approval for our web app to acess their Intasgram their Instagram Account
 After getting approval we are now going to have the information so that we can play with it.
  -->
-		<a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI?>&response_type=code">LOGIN</a>
+		<a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI?>&response_type=code">Login</a>
 	</body>
 </html>
